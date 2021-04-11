b19db335c979730a28b7ed788808940e88014aec
Rodrigo Soares <rodrigo.soares@navi.com.br>
2021-04-11 19:23:52 -0300

    Created UserItems DTO

--- app/controllers.py
+++ app/controllers.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
+from dto import UserInformation
+
 
 router = APIRouter(prefix="/insurance", tags=["insurance"])
 
 
 @router.post("/risk")
-async def calculate_risk():
+async def calculate_risk(user_information: UserInformation):
     return [{"username": "Rick"}, {"username": "Morty"}]
--- app/dto.py
+++ app/dto.py
@@ -1,11 +1,57 @@
-from pydantic import BaseModel
+from pydantic import (
+    BaseModel,
+    Field,
+    ValidationError
+)
+from enum import Enum
+from typing import Optional, List
+from typing_extensions import TypedDict
+
+
+class MaritalStatus(str, Enum):
+    married = "married"
+    single = "single"
+
+
+class OwnershipStatus(str, Enum):
+    owned = "owned"
+    mortgaged = "mortgaged"
+
+
+class House(TypedDict):
+    ownership_status: OwnershipStatus
+
+
+class Vehicle(TypedDict):
+    year: int = Field(
+        ...,
+        ge=0,
+        description="Must be a valid year"
+    )
 
 
 class UserInformation(BaseModel):
-    age: int,
-    dependents: int,
-    house: {"ownership_status": "owned"},
-    income: int,
-    marital_status: "married",
-    risk_questions: [0, 1, 0],
-    vehicle: {"year": 2018}
+    age: int = Field(
+        ...,
+        ge=0,
+        description="The age must be greater than or equal to 0"
+    )
+    dependents: int = Field(
+        ...,
+        ge=0,
+        description="Dependents must be greater than or equal to 0"
+    )
+    income: int = Field(
+        ...,
+        ge=0,
+        description="Income must be greater than or equal to 0"
+    )
+    marital_status: MaritalStatus
+    risk_questions: List[bool] = Field(
+        ...,
+        min_items=3,
+        max_items=3,
+        description="Risk questions must have 3 itens"
+    )
+    house: Optional[House]
+    vehicle: Optional[Vehicle]
